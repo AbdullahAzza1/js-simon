d970ce1a9e73620150f269ebd23138d773118fe0
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-09 22:43:47 +0100

    step1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//generare 5 numeri casuali
+//una volta generati far partire un timer di 30 secondi
+//finito il timer scompaiono i 5 numeri 
+//scrivere su un prompt i 5 numeri generati
+//il sistema controlla se i numeri scritti sul prompt siano giusti
+
+
+
+let numeri = 0;
+
+for (let i = 0; i < numeri.length; i++) {
+    const element = array[i];
+
+}
+setTimeout(myfunction, 30000);
+
+console.log("ciao");
+console.log("come va");
+
+function myfunction() {
+    console.log("eccomi");
+}
